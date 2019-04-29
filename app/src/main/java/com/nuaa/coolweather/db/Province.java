3bfcbde5c375ae35189084a1cf96b27118c95601
LRQQQ <765844394@qq.com>
2019-04-29 17:08:37 +0800

    加入创建数据库和表的各种配置

---
+++ app/src/main/java/com/nuaa/coolweather/db/Province.java
@@ -0,0 +1,36 @@
+package com.nuaa.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+
+    private int id;    //每个实体类中都应该有的字段
+    private String provinceName;  //省名
+    private int provinceCode;  //省代号
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+
+}
